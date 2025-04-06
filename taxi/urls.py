715c250d98c30850f464b0bbfba3929f86ffc03e
Odin-max <meh84741@gmail.com>
2025-04-06 19:49:17 +0300

    3

--- taxi/urls.py
+++ taxi/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path("manufacturers/", ManufacturerListView.as_view(), name="manufacturer-list"),
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
-    path("driver/", DriverListView.as_view(), name="driver-list"),
+    path("drivers/", DriverListView.as_view(), name="driver-list"),
     path("drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"),
 ]
 
